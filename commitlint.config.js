c53a4fee00dd1ad4e8183258971b17406e1ae4d4
zhijian.wang <604723443@qq.com>
2022-04-23 01:49:48 +0800

    fff

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,7 +1,7 @@
 /*
  * @Author: wangzhijian
  * @Date: 2022-04-23 01:43:50
- * @LastEditTime: 2022-04-23 01:43:51
+ * @LastEditTime: 2022-04-23 01:49:42
  */
 module.exports = {
   extends: ['@commitlint/config-conventional']
