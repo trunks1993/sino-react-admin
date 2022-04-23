8557b4b3ef742143ff774aba27103df9546e71f7
zhijian.wang <604723443@qq.com>
2022-04-23 12:07:08 +0800

    fix(module1): v1.0.9
    修复说明

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 /*
  * @Author: wangzhijian
  * @Date: 2022-04-06 22:20:57
- * @LastEditTime: 2022-04-23 12:03:17
+ * @LastEditTime: 2022-04-23 12:06:28
  */
 import React from 'react';
 import ReactDOMClient from 'react-dom/client';
@@ -14,7 +14,7 @@ if (module && module.hot) {
 }
 
 const container: any = document.getElementById('root');
-// 新 createRoot API 暂时没有声明用any
+// 新 createRoot API 暂时没有声明使用any
 ReactDOMClient.createRoot(container).render(
   <Provider store={store}>
     <App />
