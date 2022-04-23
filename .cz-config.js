0d3f25c90ae09cea32399adfd784f2d4944b5499
zhijian.wang <604723443@qq.com>
2022-04-23 11:14:13 +0800

    feat(module1-page2): ✨feat(module1-page2): f

--- .cz-config.js
+++ .cz-config.js
@@ -1,7 +1,7 @@
 /*
  * @Author: wangzhijian
  * @Date: 2022-04-23 01:09:36
- * @LastEditTime: 2022-04-23 01:46:12
+ * @LastEditTime: 2022-04-23 11:13:16
  */
 // import { existsSync, writeFileSync, readdirSync } from 'fs';
 const { readdirSync } = require('fs');
@@ -63,12 +63,12 @@ module.exports = {
     type: "Select the type of change that you're committing:",
     scope: '\nDenote the SCOPE of this change (optional):',
     // used if allowCustomScopes is true
-    customScope: 'Denote the SCOPE of this change:',
-    subject: 'Write a SHORT, IMPERATIVE tense description of the change:\n',
-    body: 'Provide a LONGER description of the change (optional). Use "|" to break new line:\n',
-    breaking: 'List any BREAKING CHANGES (optional):\n',
-    footer: 'List any ISSUES CLOSED by this change (optional). E.g.: #31, #34:\n',
-    confirmCommit: 'Are you sure you want to proceed with the commit above?',
+    // customScope: 'Denote the SCOPE of this change:',
+    // subject: 'Write a SHORT, IMPERATIVE tense description of the change:\n',
+    // body: 'Provide a LONGER description of the change (optional). Use "|" to break new line:\n',
+    // breaking: 'List any BREAKING CHANGES (optional):\n',
+    // footer: 'List any ISSUES CLOSED by this change (optional). E.g.: #31, #34:\n',
+    // confirmCommit: 'Are you sure you want to proceed with the commit above?',
   },
 
   allowCustomScopes: false,
