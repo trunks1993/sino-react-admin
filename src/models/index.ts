00f233efb39a30fc8b444310b81d99d6f7d029f9
zhijian.wang <604723443@qq.com>
2022-04-23 10:26:39 +0800

    feat: test

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,12 +1,12 @@
 /*
  * @Author: wangzhijian
  * @Date: 2022-04-20 00:14:35
- * @LastEditTime: 2022-04-23 10:23:37
+ * @LastEditTime: 2022-04-23 10:26:30
  */
 
 export type Response<T> = [boolean, T];
 export interface Page<T> {
   list: T[];
   total: number;
-  page: number;
+  page: number
 }
