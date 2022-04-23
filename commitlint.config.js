9ecda09f2fcecbf752c0738b02797655ceddfcdb
zhijian.wang <604723443@qq.com>
2022-04-23 12:43:28 +0800

    feat(module1-page2): 测试一下
    测试卡卡

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,11 +1,11 @@
 /*
  * @Author: wangzhijian
  * @Date: 2022-04-23 01:43:50
- * @LastEditTime: 2022-04-23 12:41:06
+ * @LastEditTime: 2022-04-23 12:42:10
  */
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'body-empty': [2, 'always']
+    'body-empty': [2, 'never']
   }
 };
