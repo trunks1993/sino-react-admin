10841202496982ec45459b79c3bab7aa97c29ff9
zhijian.wang <604723443@qq.com>
2022-04-23 11:49:50 +0800

    feat(module1): v1.0.4

--- .cz-config.js
+++ .cz-config.js
@@ -1,7 +1,7 @@
 /*
  * @Author: wangzhijian
  * @Date: 2022-04-23 01:09:36
- * @LastEditTime: 2022-04-23 11:34:11
+ * @LastEditTime: 2022-04-23 11:48:46
  */
 // import { existsSync, writeFileSync, readdirSync } from 'fs';
 const { readdirSync } = require('fs');
@@ -16,18 +16,6 @@ modules.map(pathname => {
 })
 
 module.exports = {
-  // types: [
-  //   { value: "特性", name: "特性:    一个新的特性" },
-  //   { value: "修复", name: "修复:    修复一个Bug" },
-  //   { value: "文档", name: "文档:    变更的只有文档" },
-  //   { value: "格式", name: "格式:    空格, 分号等格式修复" },
-  //   { value: "重构", name: "重构:    代码重构，注意和特性、修复区分开" },
-  //   { value: "性能", name: "性能:    提升性能" },
-  //   { value: "测试", name: "测试:    添加一个测试" },
-  //   { value: "工具", name: "工具:    开发工具变动(构建、脚手架工具等)" },
-  //   { value: "回滚", name: "回滚:    代码回退" },
-  // ],
-
   types: [
     {value: 'feat',     name: 'feat:     新功能开发'},
     {value: 'fix',      name: 'fix:      修复一个Bug'},
