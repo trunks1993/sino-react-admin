73c84eec27e2b73bc38b913506332ab5c2437fad
zhijian.wang <604723443@qq.com>
2022-04-23 12:41:25 +0800

    feat(module1): v1.0.13

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,15 +1,11 @@
 /*
  * @Author: wangzhijian
  * @Date: 2022-04-23 01:43:50
- * @LastEditTime: 2022-04-23 11:53:36
+ * @LastEditTime: 2022-04-23 12:41:06
  */
 module.exports = {
   extends: ['@commitlint/config-conventional'],
-  // rules: {
-  //   'type-enum': [
-  //     2,
-  //     'always',
-  //     ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'code']
-  //   ]
-  // }
+  rules: {
+    'body-empty': [2, 'always']
+  }
 };
